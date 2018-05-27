aaf43f6693c96e5d5a36959740073a07e6268ee4
Gabriel Ribolive <gabriel.ribolive@gmail.com>
2018-05-27 18:00:20 -0300

    Update altura_ideal.py

--- altura_ideal.py
+++ altura_ideal.py
@@ -2,7 +2,7 @@
 def altIdeal(mi, ci, mmi, m, c, mm):			# para futuras conversoes 1M = 1000mm | 1C = 10mm 
 	mIdeal = mmi + (ci * 10) + (mi * 1000)		# Transformo a altura ideal passada tudo em milimetros
 	minhaAltura = mm + (c * 10) + (m * 1000)
-	erro = mIdeal * 0.01						# Nao compreendi a taxa de erro em cima do valor ideal, mas acredito ser isso
+	erro = mIdeal * 0.01				# Nao compreendi a taxa de erro em cima do valor ideal, mas acredito ser isso
 
 	if minhaAltura <= (mIdeal - erro):
 		return "sim"
