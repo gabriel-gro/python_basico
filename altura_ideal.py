a7d1686068c4d3b263ac9e588acf9a5be56ac7c7
Gabriel Ribolive <gabriel.ribolive@gmail.com>
2018-05-27 17:58:33 -0300

    Altura ideal de carro alegorico
    Diminui 1% da altura ideal indicada para ser valido

---
+++ altura_ideal.py
@@ -0,0 +1,18 @@
+
+def altIdeal(mi, ci, mmi, m, c, mm):			# para futuras conversoes 1M = 1000mm | 1C = 10mm 
+	mIdeal = mmi + (ci * 10) + (mi * 1000)		# Transformo a altura ideal passada tudo em milimetros
+	minhaAltura = mm + (c * 10) + (m * 1000)
+	erro = mIdeal * 0.01						# Nao compreendi a taxa de erro em cima do valor ideal, mas acredito ser isso
+
+	if minhaAltura <= (mIdeal - erro):
+		return "sim"
+	else:
+		return "nao"
+
+def main():
+	mi, ci, mmi = map(float, input().split())
+	m, c, mm = map(float, input().split())
+
+	print(altIdeal(mi, ci, mmi, m, c, mm))		# chamo a funcao passando os parametros ideais e os meus ja imprimindo o resultado
+
+main()
